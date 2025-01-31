4c2d2a3453015e4e65e09ed2b1657af6dc406524
narrativityy <jake_crowley@comcast.net>
2025-01-30 19:52:54 -0800

    changed size value in api call and added logging for testing purposes

--- server/routes.js
+++ server/routes.js
@@ -3,8 +3,10 @@ const router = express.Router();
 const axios = require('axios');
 
 router.post('/search', async (req, res) => {
-    axios.get(`https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey=${process.env.API_KEY}&keyword=${req.body.query}&city=${req.body.city}`)
+    axios.get(`https://app.ticketmaster.com/discovery/v2/events.json?size=10&apikey=${process.env.API_KEY}&keyword=${req.body.query}${req.body.city? '&city=' + req.body.city : ''}`)
         .then(response => {
+            // logging response for testing purposes
+            console.log("response")
             res.send(response.data)
         })
         .catch(err => {
