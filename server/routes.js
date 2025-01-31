e2ce4dceb342b101b1d9534eed6177beda212c15
narrativityy <jake_crowley@comcast.net>
2025-01-30 20:01:09 -0800

    added todo comment

--- server/routes.js
+++ server/routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const axios = require('axios');
 
 router.post('/search', async (req, res) => {
+    // TODO: remove hard coded API parameters (size, city, etc.)
     axios.get(`https://app.ticketmaster.com/discovery/v2/events.json?size=10&apikey=${process.env.API_KEY}&keyword=${req.body.query}${req.body.city? '&city=' + req.body.city : ''}`)
         .then(response => {
             // logging response for testing purposes
